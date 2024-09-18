ded998e029ad429e94ed9f0ed2442e31385efb33
wangkj <>
2024-09-18 15:58:02 +0800

    update

--- src/dnf-wiki/游侠网/getWeaponData.js
+++ src/dnf-wiki/游侠网/getWeaponData.js
@@ -20,25 +20,35 @@ async function getWeaponDetail(weaponId) {
       attrList.push($(el).text())
     }
   })
-  // console.log(attrList)
+  console.log(attrList)
 
   const weaponDetail = {
-    icon: attrList[0],
-    name: attrList[1],
-    type: attrList[3],
-    level: attrList[5],
-    for: attrList[7],
-    liliang: attrList[9]==='无'?'':attrList[9],
-    zhili: attrList[11]==='无'?'':attrList[11],
-    wuligongjili: attrList[14],
-    mofagongjili: attrList[16],
-    affect: attrList[19],
+    icon: attrList[0],//
+    name: attrList[1],// 无影剑艾雷诺
+    for: attrList[7],// '鬼剑士'
+    type: attrList[3],// 短剑、太刀
+    level: attrList[5],// 50
+    // 基本信息
+    attck_speed: '普通',
+    wuligongjili: attrList[13],//+582
+    mofagongjili: attrList[15],//+706
+    liliang: attrList[9]==='无'?'':attrList[9], //56
+    zhili: attrList[11]==='无'?'':attrList[11],//60
+    tili: '-33',//60
+    jingshen: '-33',
+    shifang_speed: '+2%',
+    mofabaoji_rate: '+2%',
+    mingzhong_rate: '-1%',
+    mingzhong: '+214',
+
+    // 效果
+    affect: attrList[19],//
   }
   console.log(JSON.stringify(weaponDetail))
   return weaponDetail
 }
 
-// getWeaponDetail('wq3')
+getWeaponDetail('wq3')
 
 async function getAllWeaponDetail() {
   const resList = [] // 存放结果的数组
@@ -59,4 +69,4 @@ async function getAllWeaponDetail() {
   }
 }
 
-getAllWeaponDetail()
+// getAllWeaponDetail()
---
+++ src/dnf-wiki/游侠网/saveData/单个武器item.js
@@ -0,0 +1,12 @@
+const weaponDetail = {
+  icon: attrList[0],//
+  name: attrList[1],// 无影剑艾雷诺
+  type: attrList[3],// 短剑、太刀
+  level: attrList[5],// 50
+  for: attrList[7],// '鬼剑士'
+  liliang: attrList[9]==='无'?'':attrList[9], //56
+  zhili: attrList[11]==='无'?'':attrList[11],//60
+  wuligongjili: attrList[13],//
+  mofagongjili: attrList[15],//
+  affect: attrList[19],//
+}
